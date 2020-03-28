aaeb5e8203b63978369f4c5d7739dbae46392aa6
Frank Scholz <f.scholz85@outlook.de>
2020-03-28 14:10:53 +0100

    Rotation beim FoodSpawn

--- Assets/Scripts/Food/FoodSpawner.cs
+++ Assets/Scripts/Food/FoodSpawner.cs
@@ -29,8 +29,14 @@ public class FoodSpawner : MonoBehaviour
     {
         Food nextFood = foodSelector.GetFood();
         if (nextFood != null)
-            Instantiate(nextFood.gameObject, Vector3.zero, Quaternion.identity);
+            Instantiate(nextFood.gameObject, Vector3.zero, GetRandomRotation());
+
         return nextFood;
     }
 
+
+    private Quaternion GetRandomRotation()
+    {
+        return new Quaternion(Quaternion.identity.x, Random.Range(0.0f, 1.0f), Quaternion.identity.z, Quaternion.identity.w);
+    }
 }
