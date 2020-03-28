cbc69e76420da33dca97697355ab6b066ea49228
Frank Scholz <f.scholz85@outlook.de>
2020-03-28 21:07:11 +0100

    HomelessOverview

---
+++ Assets/Scripts/Homeless/HomelessDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class HomelessDisplay : MonoBehaviour
+{
+    public HomelessPanel HomelessPanel;
+
+    public Image homelessPortrait;
+    public Text homelessName;
+    public Slider homelessHunger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        homelessPortrait.sprite = HomelessPanel.portrait;
+        homelessName.text = HomelessPanel.homelessName;
+        homelessHunger.value = HomelessPanel.hunger;
+    }
+}
---
+++ Assets/Scripts/Homeless/HomelessPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Homeless Panel", menuName ="Homeless")]
+public class HomelessPanel : ScriptableObject
+{
+    public Sprite portrait;
+    public string homelessName;
+    public float hunger;
+}
