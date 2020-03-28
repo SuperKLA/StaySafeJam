49c7dd20f4e73d3f36901398995fb1cd71c8c570
Konstantin Lapin <xKosta.1987@gmail.com>
2020-03-28 09:04:27 +0100

    Schneiden Demo test

---
+++ Assets/Scripts/DTO/DTOConfig.cs
@@ -0,0 +1,17 @@
+﻿﻿using UnityEngine;
+
+namespace SSJ.DTO
+{
+    public class DTOConfig<T_DTO_Out> : MonoBehaviour
+    {
+        public virtual T_DTO_Out ToDTO()
+        {
+            return default(T_DTO_Out);
+        }
+        
+        public virtual void Setup()
+        {
+            
+        }
+    }
+}
