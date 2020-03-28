d286875ce84e2b7e0587508f840489ec53e55f5c
Frank Scholz <f.scholz85@outlook.de>
2020-03-28 11:51:28 +0100

    FoodSelection für den FoodSpawn, ohne Eingrenzungen oder Chancen.

---
+++ Assets/Scripts/Food/FoodSelector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSelector : MonoBehaviour
+{
+    public FoodConfig foodConfig;
+
+    private List<Food> notEatenFoods = new List<Food>();
+
+    void Start()
+    {
+        notEatenFoods.AddRange(foodConfig.foodList);
+    }
+
+    public Food GetFood()
+    {
+        int index = Random.Range(0, notEatenFoods.Count - 1);
+        Food selectedFood = notEatenFoods[index];
+        notEatenFoods.RemoveAt(index);
+        return selectedFood;
+    }
+}
