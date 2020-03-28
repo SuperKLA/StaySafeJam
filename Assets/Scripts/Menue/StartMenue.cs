fd81b7e237dd45d5c5dbda8868a52e56082f2cc6
Frank Scholz <f.scholz85@outlook.de>
2020-03-28 18:46:40 +0100

    StartMenue und EndScreen

---
+++ Assets/Scripts/Menue/StartMenue.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenue : MonoBehaviour
+{
+    public string scenePlayClick;
+
+    private AudioFiles audioFiles;
+    private AudioSource audioSource;
+    private Animator animator;
+
+    void Start()
+    {
+        StartBackgroundMusik();
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayBtnCLick()
+    {
+        animator.SetTrigger("LoadScene");
+    }
+
+    public void ExitBtnClick()
+    {
+        Application.Quit();
+    }
+
+    public void PlayClickSound()
+    {
+        audioSource.PlayOneShot(audioFiles.clickSound);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scenePlayClick);
+    }
+
+    private void StartBackgroundMusik()
+    {
+        audioFiles = FindObjectOfType<AudioFiles>();
+        audioSource = FindObjectOfType<AudioSource>();
+
+        audioSource.clip = audioFiles.startMenueBackgroundSound;
+        audioSource.loop = true;
+        audioSource.Play();
+    }
+}
