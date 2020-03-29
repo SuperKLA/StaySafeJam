ec7484fe39b61dfd133cddc82bd6ef65e3519c69
Konstantin Lapin <xKosta.1987@gmail.com>
2020-03-29 13:36:53 +0200

    Parameter werden behandelt

--- Assets/Scripts/MainMenuMusic.cs
+++ Assets/Scripts/MainMenuMusic.cs
@@ -4,19 +4,24 @@ public class MainMenuMusic : MonoBehaviour
 {
     public static MainMenuMusic Current;
     public FMOD.Studio.EventInstance AUDIO_MainMusic;
+    public bool InitDone;
 
     void Start()
     {
         Current = this;
 
+        if (this.InitDone) return;
+        
         this.AUDIO_MainMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/music_main_loop");
         this.AUDIO_MainMusic.start();
+
+        this.InitDone = true;
     }
 
     public void SetBadMood(float val)
     {
-        if (this.AUDIO_MainMusic == null) return;
+        if (!this.InitDone) return;
         
-        this.AUDIO_MainMusic.setParameterByName("gameplay_bad", val);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("gameplay_bad", val);
     }
 }
