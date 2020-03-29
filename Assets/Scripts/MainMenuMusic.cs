8657bdc818c03a2dd2c7ca2a63ab359a61dcb848
Konstantin Lapin <xKosta.1987@gmail.com>
2020-03-29 13:10:46 +0200

    WIP

---
+++ Assets/Scripts/MainMenuMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MainMenuMusic : MonoBehaviour
+{
+    public static MainMenuMusic Current;
+    public FMOD.Studio.EventInstance AUDIO_MainMusic;
+
+    void Start()
+    {
+        Current = this;
+
+        this.AUDIO_MainMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/music_main_loop");
+        this.AUDIO_MainMusic.start();
+    }
+
+    public void SetBadMood(float val)
+    {
+        if (this.AUDIO_MainMusic == null) return;
+        
+        this.AUDIO_MainMusic.setParameterByName("gameplay_bad", val);
+    }
+}
