00d9392bcff5786fe343c0495d0312f9b63bd7bb
Konstantin Lapin <xKosta.1987@gmail.com>
2020-03-29 11:15:40 +0200

    Minimum game loop

---
+++ Assets/Scripts/EssenverteilenUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EssenverteilenUI : MonoBehaviour
+{
+    public void NextClick()
+    {
+        var allCups = CupComeCloserManager.Current.Cups;
+        for (int c = 0; c < allCups.Count; c++)
+        {
+            var cup = allCups[c];
+            cup.FeedYourHomeless();
+        }
+
+        if (HomelessManager.Current.IsSomeDead())
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("HungerScene");    
+        }
+    }
+    
+}
